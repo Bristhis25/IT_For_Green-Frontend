381740456a559655522ea65d04836dc3190290dd
Bristhis <brice@PCSIVEL.>
2025-04-06 22:12:15 +0200

    mise a jour

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            Skill4Mind Learn
           </a>{" "}
         </div>
 
@@ -29,37 +29,22 @@ export const Navigation = (props) => {
           <ul className="nav navbar-nav navbar-right">
             <li>
               <a href="#features" className="page-scroll">
-                Features
+                A Propos
               </a>
             </li>
             <li>
               <a href="#about" className="page-scroll">
-                About
+                Nos domaines
               </a>
             </li>
             <li>
               <a href="#services" className="page-scroll">
-                Services
+                Contacts
               </a>
             </li>
             <li>
               <a href="#portfolio" className="page-scroll">
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href="#testimonials" className="page-scroll">
-                Testimonials
-              </a>
-            </li>
-            <li>
-              <a href="#team" className="page-scroll">
-                Team
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="page-scroll">
-                Contact
+                Site officiel
               </a>
             </li>
           </ul>
